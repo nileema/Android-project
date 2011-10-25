659854ccbfd7bddca46347055a2ac3c268887fad
Nileema Shingte <nileema.shingte@gmail.com>
2011-10-25 16:09:23 -0400

    changes

--- HelloAndroid/gen/example/HelloAndroid/R.java
+++ HelloAndroid/gen/example/HelloAndroid/R.java
@@ -11,9 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int footprint=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
+        public static final int footprint_image=0x7f050001;
         public static final int textview=0x7f050000;
     }
     public static final class layout {
--- HelloAndroid/src/example/HelloAndroid/HelloAndroidActivityTest.java
+++ HelloAndroid/src/example/HelloAndroid/HelloAndroidActivityTest.java
@@ -1,21 +1,28 @@
 package example.HelloAndroid;
 
+import java.util.Iterator;
 import java.util.List;
 
+
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 
-public class HelloAndroidActivity extends Activity {
+public class HelloAndroidActivityTest extends Activity {
 	/** Called when the activity is first created. */
 	private static final String TAG = "HeloWorldActivity";
+	Activity a = this;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,8 @@ public class HelloAndroidActivity extends Activity {
         LocationListener locationListener = new LocationListener() {
 	        public void onLocationChanged(Location location) {
 	          Log.d(TAG, "+++++++++++Location changed in first app: " + location.toString());
+	          Log.d(TAG, "*************" + a.getParent() + getCallingActivity());
+	          
 	        }
           public void onStatusChanged(String provider, int status, Bundle extras) {Log.d(TAG, "===========Status changed.");}
 
@@ -40,17 +49,44 @@ public class HelloAndroidActivity extends Activity {
           
           
         };
+        Log.d(TAG, "========== this classs is: " + this.getClass());
+        Log.d(TAG, "************" + locationListener.getClass()+ "    " +
+        		locationListener.getClass().getEnclosingClass() + "    " + locationListener.getClass().getEnclosingClass().getEnclosingClass());
+        locationListener.getClass().getPackage();
+        
+        
         // Register the listener with the Location Manager to receive location updates
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         
         PackageManager pm = getPackageManager();
+        
         //get a list of installed apps.
         List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
 
         Log.d(TAG, "=== list of installed apps ==== ");
         for (ApplicationInfo packageInfo : packages) {
-        	Log.d(TAG, packageInfo.packageName);
+        	Drawable d = packageInfo.loadIcon(pm);
+        	Log.d(TAG, packageInfo.packageName + "_____" + packageInfo.loadLabel(pm));
 
         }// the getLaunchIntentForPackage returns an intent that you can use with startActivity() 
+        
+        Log.d(TAG, "=== list of running activities ==== ");
+        ActivityManager am = (ActivityManager)this.getSystemService(ACTIVITY_SERVICE);
+        List l = am.getRunningAppProcesses();
+        Iterator i = l.iterator();
+        while(i.hasNext()) {
+          ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo)(i.next());
+          try {
+            CharSequence c = pm.getApplicationLabel(pm.getApplicationInfo(info.processName, PackageManager.GET_META_DATA));
+            Log.d("nileema", c.toString());
+//            Drawable d = pm.getApplicationIcon(info.processName);
+//            ImageView iv = (ImageView) findViewById(R.id.footprint_image);
+//            iv.setImageDrawable(d);
+//            iv.setVisibility(View.VISIBLE);
+            
+          }catch(Exception e) {
+            //Name Not FOund Exception
+          }
+        }
       }        
 }
